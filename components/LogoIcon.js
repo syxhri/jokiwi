bf33fef2f43b38eb9373df7801a49defaaf3465d
syxhri <15201425001@mahasiswa.ung.ac.id>
2026-01-10 16:23:08 +0800

    update and add more logo style

--- components/LogoIcon.js
+++ components/LogoIcon.js
@@ -1,3 +1,31 @@
-export default function LogoIcon({ className = "h-8 w-8" }) {
-  return <img src="/logo-jokiwi.svg" alt="Jokiwi" className={className} />;
+"use client";
+
+import Image from "next/image";
+
+const LOGO_SOURCES = {
+  normal: "/logo-jokiwi.svg",
+  white: "/logo-jokiwi-white.svg",
+  flat: "/logo-jokiwi-flat.svg",
+  flatWhite: "/logo-jokiwi-flat-white.svg",
+};
+
+export default function JokiwiLogo({
+  variant = "flat",
+  size = 32,
+  className = "",
+  alt = "Logo Jokiwi",
+  ...rest
+}) {
+  const src = LOGO_SOURCES[variant] ?? LOGO_SOURCES.gradient;
+
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={size}
+      height={size}
+      className={className}
+      {...rest}
+    />
+  );
 }
